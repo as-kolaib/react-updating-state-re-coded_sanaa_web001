f56a84e506dfdb9a0e07e182d10391dbbae3e316
Asma Kolaib <as.kolaib@gmail.com>
2020-09-14 12:27:38 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick';
 ReactDOM.render(
- <ClickityClick />,
+<div><ClickityClick /></div>,
   document.getElementById('root')
 );
